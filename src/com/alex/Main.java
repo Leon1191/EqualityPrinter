e17fee49dcdaaabfea55379189284750fb2d41c3
Leo <stbirb@yandex.ru>
2019-05-09 15:54:42 +0300

    Initial commit

---
+++ src/com/alex/Main.java
@@ -0,0 +1,19 @@
+package com.alex;
+
+public class Main {
+    public static void main(String[] args) {
+        printEqual(-2,2,3);
+
+    }
+
+    public static void printEqual(int a, int b, int c) {
+        if (a < 0 || b < 0 || c < 0) {
+            System.out.println("Invalid Value");
+        } else if ((a == b) && (b == c)) {
+            System.out.println("All numbers are equal");
+        } else if ((a != b) && (b != c) && (a != c)) {
+            System.out.println("All numbers are different");
+        }
+        else System.out.println("Neither all are equal of different");
+
+    }}
